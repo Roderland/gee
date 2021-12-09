499397b71662cdf7b624cd94ab405915d5d4eef7
roderland <1434453200@qq.com>
2021-12-09 23:08:01 +0800

    gee-cache 03

---
+++ gee-cache/http_test.go
@@ -0,0 +1,29 @@
+package gee_cache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func TestHTTPPool(t *testing.T) {
+	var db = map[string]string{
+		"Tom":  "630",
+		"Jack": "589",
+		"Sam":  "567",
+	}
+	NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	addr := "localhost:9999"
+	peers := NewHTTPPool(addr)
+	log.Println("geecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr, peers))
+}

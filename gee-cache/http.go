24649692cd1c9803afbaf52b8ab52054b9e26d03
roderland <1434453200@qq.com>
2021-12-11 18:50:00 +0800

    gee-cache 05

--- gee-cache/http.go
+++ gee-cache/http.go
@@ -2,16 +2,46 @@ package gee_cache
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
 const defaultBasePath = "/_geecache/"
+const defaultReplicas = 50
+
+var _ PeerPicker = (*HTTPPool)(nil)
+
+type HttpGetter struct {
+	baseUrl string
+}
+
+func (h *HttpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf("%v%v/%v", h.baseUrl, url.QueryEscape(group), url.QueryEscape(key))
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return bytes, nil
+}
 
 type HTTPPool struct {
-	self     string
-	basePath string
+	self        string
+	basePath    string
+	mu          sync.Mutex
+	peers       *Map
+	httpGetters map[string]*HttpGetter
 }
 
 func NewHTTPPool(self string) *HTTPPool {
@@ -48,4 +78,25 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.b)
+}
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = NewMap(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*HttpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &HttpGetter{baseUrl: peer + p.basePath}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
 }
--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module gee
 go 1.15
 
 require gee_web v0.0.0
+require gee_cache v0.0.0
 
 replace gee_web => ./gee-web
+replace gee_cache => ./gee-cache

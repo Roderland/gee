40fe11cbd162374a8232d0189e4ef2f0b3caf8f0
roderland <1434453200@qq.com>
2022-01-01 14:25:08 +0800

    gee-orm 06

--- gee-orm/orm.go
+++ gee-orm/orm.go
@@ -12,6 +12,8 @@ type Engine struct {
 	dialect dialect.Dialect
 }
 
+type TxFunc func(*session.Session) (interface{}, error)
+
 func NewEngine(driver, source string) (e *Engine, err error) {
 	db, err := sql.Open(driver, source)
 	if err != nil {
@@ -41,4 +43,22 @@ func (e *Engine) Close() {
 
 func (e *Engine) NewSession() *session.Session {
 	return session.New(e.db, e.dialect)
+}
+
+func (e *Engine) Transaction(f TxFunc) (result interface{}, err error) {
+	s := e.NewSession()
+	if err = s.Begin(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = s.RollBack()
+			panic(p)
+		} else if err != nil {
+			_ = s.RollBack()
+		} else {
+			err = s.Commit()
+		}
+	}()
+	return f(s)
 }
--- gee-orm/session/raw.go
+++ gee-orm/session/raw.go
@@ -16,6 +16,23 @@ type Session struct {
 	dialect  dialect.Dialect
 	refTable *schema.Schema
 	clause   clause.Clause
+	tx       *sql.Tx
+}
+
+type CommonDB interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+var _ CommonDB = (*sql.DB)(nil)
+var _ CommonDB = (*sql.Tx)(nil)
+
+func (s *Session) DB() CommonDB {
+	if s.tx != nil {
+		return s.tx
+	}
+	return s.db
 }
 
 func New(db *sql.DB, dialect dialect.Dialect) *Session {
@@ -29,10 +46,6 @@ func (s *Session) Clear() {
 	s.sql.Reset()
 	s.sqlVars = nil
 	s.clause = clause.Clause{}
-}
-
-func (s *Session) DB() *sql.DB {
-	return s.db
 }
 
 func (s *Session) Raw(sql string, values ...interface{}) *Session {
